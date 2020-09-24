4c68d4bfd58d27ddc6c63c4925d9272a6671f832
RZucconi <romain.zucconi@gmail.com>
2020-09-24 15:32:01 +0200

    add my tech challenge web site

---
+++ script.js
@@ -0,0 +1,7 @@
+let message="Thank you! Your message has been successfully transmitted, King Aetess and his court will reply to you as soon as possible.";
+
+document.getElementById("contactForm")
+        .addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert(message);
+});
